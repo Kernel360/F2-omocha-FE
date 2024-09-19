56705f351b5bc5d0283e12c5f8e681120412514b
해진 <86779590+haejinyun@users.noreply.github.com>
2024-09-19 15:05:45 +0900

    [FEAT] 경매 카드 구현 (#4)
    * feat: next, storybook svg webpack 설정 추가
    
    * feat: AuctionCard  구현 및 스토리북 연동
    
    * feat: hover시 그림자 추가
    
    * fix: api 없음 이슈로 생기는 에러 처리
    
    * feat: image 속성 추가
    
    * feat: 현재가 추가

---
+++ src/components/AuctionCard/AuctionCard.css.ts
@@ -0,0 +1,84 @@
+import { style } from '@vanilla-extract/css';
+
+export const cardWrapper = style({
+  position: 'relative',
+  display: 'flex',
+  flexDirection: 'column',
+  width: '280px',
+  borderRadius: '12px',
+  border: '1px solid rgba(229, 229, 229, 1)',
+  cursor: 'pointer',
+  ':hover': {
+    boxShadow: '0 0 8px rgba(0, 0, 0, 0.1)',
+  },
+});
+
+export const heartStyle = style({
+  position: 'absolute',
+  top: '12px',
+  right: '12px',
+  width: '30px',
+  height: '30px',
+});
+
+export const cardImage = style({
+  borderRadius: '12px 12px 0 0',
+  fontSize: '18px',
+  lineHeight: '24px',
+  fontWeight: '24px',
+});
+
+export const cardContent = style({
+  padding: '12px',
+});
+
+export const cardTitle = style({
+  display: 'inline-block',
+  width: '100%',
+  overflow: 'hidden',
+  textOverflow: 'ellipsis',
+  whiteSpace: 'nowrap',
+  fontSize: '18px',
+  lineHeight: '24px',
+  fontWeight: '24px',
+});
+
+export const division = style({
+  color: 'rgba(229, 229, 229, 1)',
+});
+
+export const cardFlex = style({
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+});
+
+export const cardFlexColor = style({
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  color: 'rgba(200, 0, 0, 1)',
+});
+
+export const cardTimeWrapper = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '8px',
+});
+
+export const dim = style({
+  position: 'absolute',
+  top: '0',
+  left: '0',
+  width: '100%',
+  height: '100%',
+  backgroundColor: 'rgba(0, 0, 0, 0.6)',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  fontSize: '24px',
+  color: 'rgba(255, 255, 255, 1)',
+  fontWeight: 'bold',
+  borderRadius: '12px',
+  zIndex: 1,
+});
---
+++ src/components/AuctionCard/index.tsx
@@ -0,0 +1,63 @@
+import Image from 'next/image';
+import HeartIcon from '@/assets/svg/heart.svg';
+import * as S from './AuctionCard.css';
+
+interface AuctionCardProps {
+  isExpired: boolean;
+  image: string;
+  title: string;
+  isLike: boolean;
+  startPrice: number;
+  startTime: string;
+  endTime: string;
+  nowPrice?: number;
+}
+
+function AuctionCard(SAMPLE: AuctionCardProps) {
+  const { isExpired, image, title, isLike, startPrice, startTime, endTime, nowPrice } = SAMPLE;
+  // time 어떻게 들어오는지 확인하고 validation 주어야함.
+
+  return (
+    <div className={S.cardWrapper}>
+      {isExpired && <div className={S.dim}>종료된 경매입니다.</div>}
+      <HeartIcon className={S.heartStyle} stroke="red" fill={isLike ? 'red' : 'none'} />
+      <Image
+        src={image || 'https://via.placeholder.com/280x200'}
+        alt="Auction Image"
+        width={280}
+        height={200}
+        className={S.cardImage}
+      />
+      <div className={S.cardContent}>
+        <span className={S.cardTitle}>{title}</span>
+        <hr className={S.division} />
+        <div className={S.cardTimeWrapper}>
+          <div className={S.cardFlex}>
+            <span>시작가(KRW)</span>
+            <span>{startPrice ? startPrice.toLocaleString('ko-KR') : '3,000'}원</span>
+            {/* 위 삼항연산자는 startPrice가 없을 경우 3,000원으로 표시하도록 함. 나중에 없어질 것임. */}
+          </div>
+          {nowPrice && (
+            <div className={S.cardFlexColor}>
+              <span>현재가(KRW)</span>
+              <span>{nowPrice.toLocaleString('ko-KR')}원</span>
+            </div>
+          )}
+        </div>
+        <hr className={S.division} />
+        <div className={S.cardTimeWrapper}>
+          <div className={S.cardFlex}>
+            <span>시작</span>
+            <span>{startTime} (KST)</span>
+          </div>
+          <div className={S.cardFlex}>
+            <span>종료</span>
+            <span>{endTime} (KST)</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AuctionCard;
