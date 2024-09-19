94cd800a8e0fac876985c08d69ffe8c2174bea6f
Daeeui Kim <kimeodml@gmail.com>
2024-09-19 16:51:43 +0900

    feat: PC Footer 구현

---
+++ src/components/Footer/Footer.css.ts
@@ -0,0 +1,34 @@
+import { style } from '@vanilla-extract/css';
+
+export const container = style({
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-around',
+  height: '130px',
+  padding: '0 30px',
+  backgroundColor: 'rgb(0, 0, 0, 0.9)',
+  color: 'white',
+});
+
+export const topFooter = style({
+  display: 'flex',
+  justifyContent: 'space-around',
+});
+
+export const categoryList = style({
+  display: 'flex',
+  listStyle: 'none',
+  fontSize: '15px',
+  fontWeight: 'bold',
+  alignItems: 'center',
+});
+
+export const category = style({
+  width: '100px',
+});
+
+export const bottomFooter = style({
+  display: 'flex',
+  justifyContent: 'center',
+  fontSize: '12px',
+});
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,5 +1,20 @@
+import * as S from '@/components/Footer/Footer.css';
+
 function Footer() {
-  return <div>Footer 입니다.</div>;
+  return (
+    <div className={S.container}>
+      <div className={S.topFooter}>
+        <h2>LOGO</h2>
+        <ul className={S.categoryList}>
+          <li className={S.category}>Home</li>
+          <li className={S.category}>Normal</li>
+          {/* <li className={S.category}>Live</li> */}
+          <li className={S.category}>How To</li>
+        </ul>
+      </div>
+      <div className={S.bottomFooter}>Copyright ⓒ Omocha. All Rights Reserved</div>
+    </div>
+  );
 }
 
 export default Footer;
