d1ce83443a2d0366e1de35907186e33117478c64
Daeeui Kim <kimeodml@gmail.com>
2024-09-20 17:29:15 +0900

    [FEAT] 공용 레이아웃 헤더/푸터 구현 (#5)
    * refactor: global css 수정 및 viewport 추가
    
    * feat: PC header 구현
    
    * style: global css 추가
    
    * feat: PC Footer 구현
    
    * refactor: 카테고리 static으로 변경, acitve link로 수정
    
    * refactor: id 수정
    
    * refactor: 폴더 및 파일명 변경, 주석 제거
    
    * style: button 스타일 글로벌 css에 추가
    
    * style: 스타일 수정 및 제거
    
    * feat: 로고에 onClick 추가
    
    * style: maxWidth 설정
    
    * style: footer width 100%로 변경

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import { MAIN_CATEGORY, SUB_CATEGORY } from '@/static/category';
+import Link from 'next/link';
+import { usePathname, useRouter } from 'next/navigation';
+import * as S from '@/components/Header/Header.css';
+
+function Header() {
+  const pathname = usePathname();
+  const router = useRouter();
+
+  return (
+    <div className={S.container}>
+      <section className={S.topHeader}>
+        <button className={S.logoButton} type="button" onClick={() => router.push('/')}>
+          LOGO
+        </button>
+        <div className={S.topCategory}>
+          {SUB_CATEGORY.map(category => (
+            <Link
+              className={S.categoryLink}
+              key={category.id}
+              href={category.path}
+              style={{ color: category.path === pathname ? 'red' : 'black' }}
+            >
+              {category.name}
+            </Link>
+          ))}
+        </div>
+      </section>
+      <section className={S.bottomHeader}>
+        {MAIN_CATEGORY.map(category => (
+          <Link
+            className={S.categoryLink}
+            key={category.id}
+            href={category.path}
+            style={{ color: category.path === pathname ? 'red' : 'black' }}
+          >
+            {category.name}
+          </Link>
+        ))}
+      </section>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/static/category.ts
@@ -0,0 +1,14 @@
+export const SUB_CATEGORY = [
+  { id: 1, name: '게시물 업로드', path: '/upload' },
+  { id: 2, name: '마이페이지', path: '/mypage' },
+  { id: 3, name: '찜', path: '/mypage/heart' },
+  { id: 4, name: '알림', path: '/notice' },
+  { id: 5, name: '로그인', path: '/login' },
+];
+
+export const MAIN_CATEGORY = [
+  { id: 1, name: 'Home', path: '/' },
+  { id: 2, name: 'Normal', path: '/basicauction' },
+  { id: 3, name: 'Live', path: '/liveauction' },
+  { id: 4, name: 'How To', path: '/howto' },
+];
