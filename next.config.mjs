5c618ae9fc959345c8205bb4ece99876c0178c3c
Daeeui Kim <kimeodml@gmail.com>
2024-09-20 12:19:02 +0900

    Revert "[DOCS] 깃헙 action workflow (#7)" (#8)
    This reverts commit 1965a8e08d4cc21e354d48acb14c82b297c9e113.

--- next.config.mjs
+++ next.config.mjs
@@ -3,17 +3,6 @@ import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin';
 const withVanillaExtract = createVanillaExtractPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  webpack: config => {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ['@svgr/webpack'],
-    });
-    return config;
-  },
-};
+const nextConfig = {};
 
 export default withVanillaExtract(nextConfig);
