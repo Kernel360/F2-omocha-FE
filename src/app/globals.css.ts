8f0bfbbfe9b05b44ce37ce5165d941fe4fd36380
Daeeui Kim <kimeodml@gmail.com>
2024-09-20 15:03:09 +0900

    style: button 스타일 글로벌 css에 추가

--- src/app/globals.css.ts
+++ src/app/globals.css.ts
@@ -1,6 +1,6 @@
 import { globalStyle } from '@vanilla-extract/css';
 
-globalStyle('html, body, h1, h2, h3, h4, p, ul, ol, li', {
+globalStyle('html, body, h1, h2, h3, h4, button, p, ul, ol, li', {
   margin: 0,
   padding: 0,
 });
@@ -14,4 +14,9 @@ globalStyle('html, body', {
 /* Chrome, Safari, and Opera */
 globalStyle('::-webkit-scrollbar', {
   display: 'none',
+});
+
+globalStyle('button', {
+  border: 'none',
+  backgroundColor: 'transparent',
 });
