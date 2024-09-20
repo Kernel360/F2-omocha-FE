4819822fda705a684a79e6da98f8b2b5978da106
dbshaejin <dbshaejin@gmail.com>
2024-09-20 16:15:17 +0900

    feat: tab 구현 및 스토리북 등록

---
+++ src/components/TabsLayout/Tabs.css.ts
@@ -0,0 +1,29 @@
+import { style } from '@vanilla-extract/css';
+
+export const tabsRoot = style({
+  display: 'flex',
+  width: '100%',
+  flexDirection: 'column',
+  gap: '10px',
+});
+export const tabsList = style({
+  display: 'flex',
+});
+
+export const tabsTrigger = style({
+  padding: '10px 20px',
+  width: '100%',
+  backgroundColor: 'transparent',
+  border: 'none',
+  cursor: 'pointer',
+  transition: 'all 0.2s ease',
+  borderBottom: '2px solid black',
+
+  selectors: {
+    '&[data-state="active"]': {
+      borderBottom: '2px solid red',
+    },
+  },
+});
+
+export const tabsContent = style({});
