b304ca2f41a280ca491f2692fe4be869e21b0fcd
dbshaejin <dbshaejin@gmail.com>
2024-09-24 16:04:51 +0900

    feat: 회원가입 페이지 구현 및 로직 구현

---
+++ src/app/join/Join.css.ts
@@ -0,0 +1,83 @@
+import { style } from '@vanilla-extract/css';
+
+export const container = style({
+  display: 'flex',
+  flexDirection: 'column',
+  height: 'calc(100vh - 262px)', // 공백 추가
+  boxSizing: 'border-box',
+  paddingTop: '50px',
+  alignItems: 'center',
+});
+
+export const title = style({
+  color: 'black ',
+  textAlign: 'center',
+  fontSize: '32px',
+});
+
+export const inputSection = style({
+  paddingTop: '50px',
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '16px',
+});
+
+export const inputLabel = style({ display: 'flex', flexDirection: 'column', gap: '6px' });
+
+export const joinInput = style({
+  display: 'flex',
+  maxWidth: '368px',
+  width: '100%',
+  borderRadius: '4px',
+  padding: '12px 16px',
+  alignItems: 'flex-start',
+  gap: '8px',
+  border: '1.5px solid black',
+  background: '#FFF',
+});
+
+export const inputValidation = style({
+  display: 'flex',
+  gap: '4px',
+  alignItems: 'center',
+  color: '#FF0000',
+  fontSize: '14px',
+  marginLeft: '8px',
+});
+
+export const error = style({
+  color: '#FF0000',
+});
+
+export const correct = style({
+  color: 'green',
+});
+
+export const submitButton = style({
+  marginTop: '30px',
+  width: '402px',
+  padding: '15px 62px',
+  background: 'black ',
+  color: '#FFF',
+  fontSize: '16px',
+  borderRadius: '4px',
+  border: 'none',
+  cursor: 'pointer',
+});
+
+export const checkButton = style({
+  display: 'flex',
+  background: 'black',
+  color: '#FFF',
+  fontSize: '14px',
+  width: '80px',
+  alignItems: 'center',
+  justifyContent: 'center',
+  borderRadius: '4px',
+});
+
+export const checkInputWrapper = style({
+  display: 'flex',
+  justifyContent: 'center ',
+  gap: '8px',
+});
--- src/app/join/page.tsx
+++ src/app/join/page.tsx
@@ -1,5 +1,157 @@
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable jsx-a11y/label-has-associated-control */
+
+'use client';
+
+import ErrorIcon from '@/assets/svg/error.svg';
+import CheckIcon from '@/assets/svg/check.svg';
+import { useForm, SubmitHandler } from 'react-hook-form';
+
+import { useState } from 'react';
+import * as S from './Join.css';
+
+type Inputs = {
+  emailRequired: string;
+  passwordRequired: string;
+  passwordCheckRequired: string;
+};
+
 function Home() {
-  return <>joins</>;
+  const {
+    register,
+    handleSubmit,
+    watch,
+    setError,
+    clearErrors,
+
+    formState: { errors },
+  } = useForm<Inputs>({
+    mode: 'onChange',
+    defaultValues: {
+      emailRequired: '',
+      passwordRequired: '',
+      passwordCheckRequired: '',
+    },
+  });
+
+  const emailValue = watch('emailRequired');
+  const passwordValue = watch('passwordRequired');
+
+  const [emailCheck, setEmailCheck] = useState(false);
+
+  const checkEmail = () => {
+    if (emailValue === '') {
+      return;
+    }
+
+    console.log('emailValue', emailValue);
+    const emailExists = false; // 실제 API를 통해 중복 여부 확인
+
+    if (emailExists) {
+      // 중복된 이메일이면 에러 설정
+      setError('emailRequired', { type: 'manual', message: '중복된 이메일이 있습니다.' });
+    } else {
+      setEmailCheck(true);
+      clearErrors('emailRequired');
+    }
+  };
+
+  const onSubmit: SubmitHandler<Inputs> = data => {
+    if (!emailCheck) {
+      setError('emailRequired', { type: 'manual', message: '이메일 중복 확인이 필요합니다.' });
+      return;
+    }
+
+    console.log(data);
+  };
+
+  return (
+    <div className={S.container}>
+      <span className={S.title}>회원가입하기</span>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className={S.inputSection}>
+          <label className={S.inputLabel}>
+            이메일
+            <div className={S.checkInputWrapper}>
+              <input
+                className={S.joinInput}
+                placeholder="이메일을 입력하세요."
+                type="email"
+                {...register('emailRequired', {
+                  required: true,
+                  onChange: () => setEmailCheck(false),
+                })}
+              />
+              <button type="button" className={S.checkButton} onClick={() => checkEmail()}>
+                중복확인
+              </button>
+            </div>
+            {errors.emailRequired && (
+              <span className={`${S.inputValidation} ${S.error}`}>
+                <ErrorIcon />
+                {errors.emailRequired.type === 'required'
+                  ? '이메일을 입력하세요.'
+                  : errors.emailRequired.message}
+              </span>
+            )}
+            {emailCheck && (
+              <span className={`${S.inputValidation} ${S.correct}`}>
+                <CheckIcon />
+                사용 가능한 이메일입니다.
+              </span>
+            )}
+          </label>
+          <label className={S.inputLabel}>
+            비밀번호
+            <input
+              className={S.joinInput}
+              placeholder="비밀번호를 입력하세요."
+              type="password"
+              {...register('passwordRequired', {
+                required: '비밀번호를 입력하세요.',
+                validate: {
+                  minLength: value =>
+                    value.length >= 8 || '비밀번호는 최소 8글자 이상이어야 합니다.',
+                  number: value => /[0-9]/.test(value) || '비밀번호에는 숫자가 포함되어야 합니다.',
+                  letter: value =>
+                    /[a-zA-Z]/.test(value) || '비밀번호에는 알파벳이 포함되어야 합니다.',
+                  specialChar: value =>
+                    /[\W_]/.test(value) || '비밀번호에는 특수문자가 포함되어야 합니다.',
+                },
+              })}
+            />
+            {errors.passwordRequired && (
+              <span className={`${S.inputValidation} ${S.error}`}>
+                <ErrorIcon />
+                {errors.passwordRequired.type === 'required'
+                  ? '비밀번호를 입력하세요.'
+                  : errors.passwordRequired.message}
+              </span>
+            )}
+          </label>
+          <label className={S.inputLabel}>
+            비밀번호 확인
+            <input
+              className={S.joinInput}
+              placeholder="비밀번호를 입력하세요."
+              type="password"
+              {...register('passwordCheckRequired', {
+                required: '비밀번호를 입력하세요.',
+                validate: value => value === passwordValue || '비밀번호가 일치하지 않습니다.',
+              })}
+            />
+            {errors.passwordCheckRequired && (
+              <span className={`${S.inputValidation} ${S.error}`}>
+                <ErrorIcon />
+                {errors.passwordCheckRequired.message}
+              </span>
+            )}
+          </label>
+        </div>
+        <input className={S.submitButton} type="submit" value="회원가입" />
+      </form>
+    </div>
+  );
 }
 
 export default Home;
