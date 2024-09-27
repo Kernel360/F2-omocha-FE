bb61bbaff697a274c29ec78971dd78f154c4a14f
Daeeui Kim <kimeodml@gmail.com>
2024-09-27 11:55:36 +0900

    feat: 스토리북에 공용 모달 추가

---
+++ src/components/Modal/Modal.css.ts
@@ -0,0 +1,20 @@
+import { style } from '@vanilla-extract/css';
+
+export const overlay = style({
+  position: 'absolute',
+  top: '0',
+  left: '0',
+  width: '100%',
+  height: '100%',
+  backgroundColor: 'rgba(0, 0, 0, 0.6)',
+});
+export const modal = style({
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  display: 'flex',
+  flexDirection: 'column',
+  width: '500px',
+  backgroundColor: 'white',
+});
