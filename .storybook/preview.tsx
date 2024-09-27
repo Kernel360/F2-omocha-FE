1e191001543df5c2e21cb3785189b92f438fba15
Daeeui Kim <kimeodml@gmail.com>
2024-09-27 11:49:28 +0900

    fix: tsx로 확장

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,4 +1,5 @@
 import type { Preview } from '@storybook/react';
+import React from 'react';
 
 const preview: Preview = {
   parameters: {
@@ -9,6 +10,14 @@ const preview: Preview = {
       },
     },
   },
+  decorators: [
+    Story => (
+      <div>
+        <Story />
+        <div id="root-portal" />
+      </div>
+    ),
+  ],
 };
 
 export default preview;
