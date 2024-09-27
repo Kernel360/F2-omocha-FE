d7c00ff5c6e35aa0be54f4edf8adf2bdae880b8c
Daeeui Kim <kimeodml@gmail.com>
2024-09-27 17:54:27 +0900

    [FEAT] 공용 모달 컴포넌트 (#27)
    * fix: tsx로 확장
    
    * feat: createPortal 전용 div 태그 추가
    
    * feat: 스토리북에 공용 모달 추가
    
    * feat: 컴포넌트를 제어할 수 있는 useDisclosure 훅 추가
    
    * feat: 공용 모달 컴포넌트 추가

---
+++ src/components/Modal/Modal.css.ts
@@ -0,0 +1,71 @@
+import { style, styleVariants } from '@vanilla-extract/css';
+
+export const overlay = style({
+  position: 'absolute',
+  top: '0',
+  left: '0',
+  width: '100%',
+  height: '100%',
+  backgroundColor: 'rgba(0, 0, 0, 0.6)',
+});
+export const modal = style({
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  display: 'flex',
+  flexDirection: 'column',
+  maxWidth: '350px',
+  width: '100%',
+  backgroundColor: 'white',
+  padding: '10px',
+  gap: '20px',
+  borderRadius: '10px',
+});
+
+export const buttonHeader = style({
+  display: 'flex',
+  justifyContent: 'flex-end',
+  border: 'none',
+  borderBottom: '1.5px solid black',
+  backgroundColor: 'transparent',
+  paddingBottom: '10px',
+});
+
+export const content = style({
+  display: 'flex',
+  justifyContent: 'center',
+  textAlign: 'center',
+  margin: '10px',
+});
+
+export const buttonFooter = style({
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'space-around',
+});
+
+export const buttonBase = style({
+  border: 'none',
+  backgroundColor: 'transparent',
+  color: 'white',
+  borderRadius: '4px',
+  maxWidth: '100px',
+  width: '100%',
+  padding: '10px 5px',
+});
+
+export const button = styleVariants({
+  left: [
+    buttonBase,
+    {
+      backgroundColor: 'rgb(196,39,39)',
+    },
+  ],
+  right: [
+    buttonBase,
+    {
+      backgroundColor: 'black',
+    },
+  ],
+});
