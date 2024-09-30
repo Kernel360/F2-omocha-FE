3afff9b8b53ccf0c83e2a53e870b8c25d5898d1b
dbshaejin <dbshaejin@gmail.com>
2024-09-30 16:45:33 +0900

    feat: 알림 사이드바 레이아웃 구현

--- src/components/SlideSideNav/SlideSideNav.css.ts
+++ src/components/SlideSideNav/SlideSideNav.css.ts
@@ -1,3 +1,36 @@
 import { style } from '@vanilla-extract/css';
 
-export const container = style({});
+export const overlay = style({
+  position: 'absolute',
+  top: '0',
+  left: '0',
+  width: '100%',
+  height: '100%',
+  backgroundColor: 'rgba(0, 0, 0, 0.6)',
+});
+
+export const container = style({
+  zIndex: 100,
+
+  backgroundColor: 'rgba(255, 255, 255, 1)',
+  position: 'absolute',
+  top: '0',
+  right: '0',
+  height: '100%',
+  width: '420px',
+  boxSizing: 'border-box',
+  padding: '30px 56px',
+});
+
+export const titleSection = style({
+  display: 'flex',
+  alignItems: 'center',
+  gap: '12px',
+});
+
+export const title = style({
+  fontSize: '20px',
+  letterSpacing: '-.3px',
+
+  textAlign: 'left',
+});
--- src/components/SlideSideNav/index.tsx
+++ src/components/SlideSideNav/index.tsx
@@ -1,11 +1,39 @@
-import * as S from './SlideSideNav.css';
+import { useEffect, useRef } from 'react';
 
-function SlideSideNav() {
+import XIcon from '@/assets/svg/x.svg';
+
+import Portal from '../Portal';
+
+import * as S from './SlideSideNav.css';
+import useOnClickOutside from '@/hooks/useOnClickOutside';
+
+interface SlideSideNavProps {
+  value: boolean;
+  onClose: () => void;
+  children: React.ReactNode;
+}
+
+function SlideSideNav(Sample: SlideSideNavProps) {
+  const { value, onClose, children } = Sample;
+
+  const sideNavRef = useRef<HTMLDivElement>(null);
+
+  useOnClickOutside(sideNavRef, onClose);
+
   return (
-    <div className={S.container}>
-      <span>SlideSideNav</span>
-      <div>dib</div>
-    </div>
+    <Portal>
+      <div className={S.overlay}>
+        <div ref={sideNavRef} className={S.container}>
+          <div className={S.titleSection}>
+            <button type="button" onClick={onClose}>
+              <XIcon />
+            </button>
+            <span className={S.title}>알림</span>
+          </div>
+          {children}
+        </div>
+      </div>
+    </Portal>
   );
 }
 
