c0deae332cc69a83b97eb51695cb6645c0e10cb0
dbshaejin <dbshaejin@gmail.com>
2024-09-30 13:01:15 +0900

    feat: 상단 header에 따른 액션 분기

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,12 +3,33 @@
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
-import { MAIN_CATEGORY, SUB_CATEGORY } from '@/static/category';
+import useBooleanState from '@/hooks/useBooleanState';
+import { MAIN_CATEGORY, SUB_CATEGORY, SubCategory } from '@/static/category';
 
 import * as S from './Header.css';
 
 function Header() {
   const pathname = usePathname();
+  const { value, setTrue } = useBooleanState(false);
+  console.log(value); // 임시 console
+
+  const headerItemAction = (headerItem: SubCategory) => {
+    switch (headerItem.name) {
+      case '알림':
+        return (
+          <button type="button" key={headerItem.id} onClick={setTrue}>
+            {headerItem.name}
+          </button>
+        );
+
+      default:
+        return (
+          <Link key={headerItem.id} href={headerItem.path!}>
+            {headerItem.name}
+          </Link>
+        );
+    }
+  };
 
   return (
     <header className={S.container}>
@@ -17,11 +38,7 @@ function Header() {
           <div className={S.logo}>LOGO</div>
         </Link>
         <div className={S.topCategory}>
-          {SUB_CATEGORY.map(category => (
-            <Link key={category.id} href={category.path}>
-              {category.name}
-            </Link>
-          ))}
+          {SUB_CATEGORY.map(category => headerItemAction(category))}
         </div>
       </section>
       <section className={S.bottomHeader}>
---
+++ src/components/SlideSideNav/index.tsx
@@ -0,0 +1,12 @@
+import * as S from './SlideSideNav.css';
+
+function SlideSideNav() {
+  return (
+    <div className={S.container}>
+      <span>SlideSideNav</span>
+      <div>dib</div>
+    </div>
+  );
+}
+
+export default SlideSideNav;
