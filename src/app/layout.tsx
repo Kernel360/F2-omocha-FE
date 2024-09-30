cdd65f7e73d83f35d77696d2954281e16f1ade1a
dbshaejin <dbshaejin@gmail.com>
2024-09-30 17:42:48 +0900

    fix: dev  충돌 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
+import { Metadata, Viewport } from 'next';
 import localFont from 'next/font/local';
 
-import * as S from '@/app/globals.css';
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
+
+import * as S from './globals.css';
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
