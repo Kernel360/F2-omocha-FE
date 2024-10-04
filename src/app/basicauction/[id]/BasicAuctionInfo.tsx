e551307d0e52cc782b7a6d462a3a0a26a128eab5
dbshaejin <dbshaejin@gmail.com>
2024-10-04 16:28:04 +0900

    fix: 타입 추론 수정

--- src/app/basicauction/[id]/BasicAuctionInfo.tsx
+++ src/app/basicauction/[id]/BasicAuctionInfo.tsx
@@ -2,9 +2,7 @@
 
 import React from 'react';
 
-import { useQuery } from '@tanstack/react-query';
-
-import { getBasicAuctionQueryFn } from '@/apis/queryFunctions/basicAuctionQueryFn';
+import useGetBasicAuction from '@/apis/queryHooks/basicAuction/useGetBasicAuction';
 import AuctionInfo from '@/components/AuctionInfo';
 
 interface TestProps {
@@ -12,19 +10,16 @@ interface TestProps {
 }
 
 function BasicAuctionInfo({ id }: TestProps) {
-  const { data } = useQuery({
-    queryKey: ['basicAuction', id],
-    queryFn: () => getBasicAuctionQueryFn(id),
-  });
+  const { data } = useGetBasicAuction(id);
 
   if (!data) return null;
 
   return (
     <AuctionInfo
-      title={data.title}
-      startPrice={data.start_price}
-      nowPrice={data.now_price}
-      endTime={data.end_date}
+      title={data.result_data.title}
+      startPrice={data.result_data.start_price}
+      nowPrice={data.result_data.now_price}
+      endTime={data.result_data.end_date}
       bidCount={0}
     />
   );
