c0c3a1f7555f06c7aeaeeba872f48d35286b7e3d
dbshaejin <dbshaejin@gmail.com>
2024-10-04 15:47:08 +0900

    chore: 불필요 주석 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 'use client';
 
-// 이걸 최대한 빼고 서버 클라이언트에서 쿼리를 가져오는 법을 알아봐야할듯.. 캐시를 통해서 어쩌구..
-
 import useGetBasicAuctionList from '@/apis/queryHooks/basicAuction/useGetBasicAuctionList';
 import AuctionCard from '@/components/AuctionCard';
 import ListLayout from '@/components/ListLayout';
