aeb12adedf26f569cd760d30d46c736a224e8fce
dbshaejin <dbshaejin@gmail.com>
2024-10-04 16:11:58 +0900

    fix: 타입 재정의 및 env파일 설정 다시 진행

--- src/apis/queryFunctions/basicAuctionQueryFn.ts
+++ src/apis/queryFunctions/basicAuctionQueryFn.ts
@@ -7,18 +7,18 @@ import {
 import apiClient from './apiClient';
 import convertQueryParamsObjectToString from './convertQueryParamsObjectToString';
 
-export const getBasicAuctionQueryFn = async (id: number) => {
-  const response = (await apiClient.get)<BasicAuctionResponse>(`/api/v1/auction/${id}`);
+export const getBasicAuctionQueryFn = async (id: number): Promise<BasicAuctionResponse> => {
+  const response = (await apiClient.get)(`/api/v1/auction/${id}`);
 
   return (await response).data.result_data;
 };
 
-export const getBasicAuctionListQueryFn = async (params: GetBasicAuctionListQueryFnProps) => {
+export const getBasicAuctionListQueryFn = async (
+  params: GetBasicAuctionListQueryFnProps,
+): Promise<AuctionListResponse> => {
   const queryString = convertQueryParamsObjectToString<GetBasicAuctionListQueryFnProps>(params);
 
-  const response = (await apiClient.get)<AuctionListResponse>(
-    `/api/v1/auction/basic-list?${queryString}`,
-  );
+  const response = await apiClient.get(`/api/v1/auction/basic-list?${queryString}`);
 
   return (await response).data;
 };
