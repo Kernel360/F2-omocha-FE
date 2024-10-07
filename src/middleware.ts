d9f8b6644a74e8e70f8f2e7d0dd8f717a36abc21
dbshaejin <dbshaejin@gmail.com>
2024-10-07 18:23:15 +0900

    feat: middleware 활용하여 비로그인 상태일때, 접근 막기

---
+++ src/middleware.ts
@@ -0,0 +1,35 @@
+import { NextResponse } from 'next/server';
+
+import type { NextRequest } from 'next/server';
+
+export function middleware(request: NextRequest) {
+  const refreshToken = request.cookies.get('refresh')?.value;
+
+  const { pathname } = request.nextUrl;
+
+  if (pathname === '/favicon.ico') {
+    return NextResponse.next(); // middleware를 통과하지 않게 함
+  }
+
+  // const accessToken = request.cookies.get('access')?.value;
+
+  // 1. accessToken을 통해서 확인
+  // 1-1. accessToken이 없으면 refreshToken있으면 re로 재발급
+  // 1-1-1. 발급 다시 받았으면 accessToken으로 다음 작업진행
+  // 1-1-2. refreshToken도 만료 되었으면 로그아웃 세션만료 되었다. 다시 로그인 해라라는 에러
+  // 1-2. refreshToken도 없으면 걍 로그인해라!
+
+  if (!refreshToken) {
+    return NextResponse.redirect(
+      new URL('/login?alert=로그인 후 이용 가능한 서비스입니다.', request.url),
+    );
+  }
+
+  // refreshToken이 존재하면 다음으로 진행 (NextResponse.next())
+  return NextResponse.next();
+}
+
+// matcher를 사용하여 '/mypage' 관련 경로에만 middleware 적용
+export const config = {
+  matcher: ['/mypage/:path*', '/create/:path*'],
+};
