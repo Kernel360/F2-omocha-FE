76467c14715401ef17f75908d3e2acad3515a1d4
dbshaejin <dbshaejin@gmail.com>
2024-10-10 17:43:14 +0900

    fix: 이전 코드 돌려놓기

--- src/apis/queryHooks/basicAuction/usePostBasicAuction.ts
+++ src/apis/queryHooks/basicAuction/usePostBasicAuction.ts
@@ -1,14 +1,17 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useRouter } from 'next/navigation';
 
 import { postBasicAuction } from '@/apis/queryFunctions/basicAuction';
 
 function usePostBasicAuction() {
   const queryClient = useQueryClient();
+  const router = useRouter();
 
   const { mutate, error } = useMutation({
     mutationFn: (param: FormData) => postBasicAuction(param),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['basicAuctionList'] });
+      router.push('/basicauction');
     },
   });
 
