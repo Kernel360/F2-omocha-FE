45f2aa702a29ea1450145f1ff8ab27fe2df95f17
dbshaejin <dbshaejin@gmail.com>
2024-10-10 11:31:08 +0900

    fix: useCountdownTimer 서버 시간 오차 이슈 해결

--- src/hooks/useCountdownTimer.ts
+++ src/hooks/useCountdownTimer.ts
@@ -1,5 +1,6 @@
 'use client';
 
+import calcRemainingTime from '@/utils/calcRemainingTime';
 import { useEffect, useState } from 'react';
 
 interface UseCountdownTimerProps {
@@ -11,23 +12,12 @@ const HOUR_IN_MILLIS = MINUTE_IN_MILLIS * 60;
 const DAY_IN_MILLIS = HOUR_IN_MILLIS * 24;
 
 function useCountdownTimer({ endTime }: UseCountdownTimerProps) {
-  const countRemainingTime = (endTimeInCountRemainingTime: Date) => {
-    const nowTime = new Date();
-
-    if (endTimeInCountRemainingTime.getTime() < nowTime.getTime()) {
-      return 0;
-    }
-
-    return endTimeInCountRemainingTime.getTime() - nowTime.getTime();
-  };
-
-  const [remainingTime, setRemainingTime] = useState<number>(() =>
-    countRemainingTime(new Date(endTime)),
-  );
+  const [remainingTime, setRemainingTime] = useState<number>(0);
 
   useEffect(() => {
+    setRemainingTime(calcRemainingTime(new Date(endTime)));
     const intervalID = setInterval(() => {
-      const remainingTimeInUseEffect = countRemainingTime(new Date(endTime));
+      const remainingTimeInUseEffect = calcRemainingTime(new Date(endTime));
 
       if (remainingTimeInUseEffect <= 0) {
         clearInterval(intervalID);
@@ -41,12 +31,12 @@ function useCountdownTimer({ endTime }: UseCountdownTimerProps) {
     return () => {
       clearInterval(intervalID);
     };
-  });
+  }, []);
 
-  const day = Math.floor(remainingTime / DAY_IN_MILLIS);
-  const hour = Math.floor((remainingTime % DAY_IN_MILLIS) / HOUR_IN_MILLIS);
-  const minute = Math.floor((remainingTime % HOUR_IN_MILLIS) / MINUTE_IN_MILLIS);
-  const second = Math.floor((remainingTime % MINUTE_IN_MILLIS) / 1000);
+  const day = Math.floor(remainingTime / DAY_IN_MILLIS) || '-';
+  const hour = Math.floor((remainingTime % DAY_IN_MILLIS) / HOUR_IN_MILLIS) || '-';
+  const minute = Math.floor((remainingTime % HOUR_IN_MILLIS) / MINUTE_IN_MILLIS) || '-';
+  const second = Math.floor((remainingTime % MINUTE_IN_MILLIS) / 1000) || '-';
   const isTimeout = remainingTime <= 0;
 
   return { isTimeout, remainingTime, day, hour, minute, second };
