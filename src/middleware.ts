3a4dc43361b71751dd2c52442c325b24b5649e43
Daeeui Kim <kimeodml@gmail.com>
2024-10-11 17:42:33 +0900

    feat: 로그인 이후 로그인페이지, 회원가입 페이지 경로 막기

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,23 +2,19 @@ import { NextResponse } from 'next/server';
 
 import type { NextRequest } from 'next/server';
 
+const afterLoginProtectedRoutes = ['/login', '/join'];
+
 export function middleware(request: NextRequest) {
+  const refreshToken = request.cookies.get('refresh')?.value;
+  const accessToken = request.cookies.get('access')?.value;
+
   const { pathname } = request.nextUrl;
 
-  if (pathname === '/favicon.ico') {
-    return NextResponse.next(); // middleware를 통과하지 않게 함
+  if (refreshToken && afterLoginProtectedRoutes.includes(pathname)) {
+    return NextResponse.redirect(new URL('/', request.url));
   }
 
-  const refreshToken = request.cookies.get('refresh')?.value;
-  // const accessToken = request.cookies.get('access')?.value;
-
-  // 1. accessToken을 통해서 확인
-  // 1-1. accessToken이 없으면 refreshToken있으면 re로 재발급
-  // 1-1-1. 발급 다시 받았으면 accessToken으로 다음 작업진행
-  // 1-1-2. refreshToken도 만료 되었으면 로그아웃 세션만료 되었다. 다시 로그인 해라라는 에러
-  // 1-2. refreshToken도 없으면 걍 로그인해라!
-
-  if (!refreshToken) {
+  if (!refreshToken && !accessToken) {
     return NextResponse.redirect(
       new URL('/login?alert=로그인 후 이용 가능한 서비스입니다.', request.url),
     );
@@ -29,5 +25,5 @@ export function middleware(request: NextRequest) {
 
 // matcher를 사용하여 지정한 경로에만 middleware 적용
 export const config = {
-  matcher: ['/mypage/:path*', '/create/:path*'],
+  matcher: ['/mypage/:path*', '/create/:path*', '/login', '/join'],
 };
