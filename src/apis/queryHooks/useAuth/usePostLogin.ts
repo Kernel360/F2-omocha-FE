7b49a4a138bd4cd6a384a73c99fb673cb400132d
dbshaejin <dbshaejin@gmail.com>
2024-10-11 10:49:46 +0900

    test: set도오저언...

--- src/apis/queryHooks/useAuth/usePostLogin.ts
+++ src/apis/queryHooks/useAuth/usePostLogin.ts
@@ -5,13 +5,20 @@ import { useRouter } from 'next/navigation';
 import { postLogin } from '@/apis/queryFunctions/Auth';
 import { LoginParams } from '@/apis/types/Auth';
 import { Response } from '@/apis/types/common';
+// import { useAuth } from '@/provider/authProvider';
 
 function usePostLogin() {
   const router = useRouter();
+  // const { token, setToken } = useAuth();
 
   const { mutate, error } = useMutation({
     mutationFn: (param: LoginParams) => postLogin(param),
-    onSuccess: () => {
+    onSuccess: (data, variable) => {
+      console.log('data', data);
+      console.log('variable', variable);
+      const cookieString = document.cookie;
+      console.log('cookieString', cookieString);
+      // 여기서 콜백으로 token을 다시 set할 수 있게 재렌더링이 일어날 수 있는 요소가 필요한데...
       router.push('/');
     },
     onError: (e: AxiosError<Response<string>>) => {
