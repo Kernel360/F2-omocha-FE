ad48cba9b0dc2e0e945e55135d3ec9cb467aabb3
Daeeui Kim <kimeodml@gmail.com>
2024-10-13 11:58:12 +0900

    fix: 무한 리다이렉트 수정

--- src/middleware.ts
+++ src/middleware.ts
@@ -14,7 +14,11 @@ export function middleware(request: NextRequest) {
     return NextResponse.redirect(new URL('/', request.url));
   }
 
-  if (!refreshToken && !accessToken) {
+  if (afterLoginProtectedRoutes.includes(pathname)) {
+    return NextResponse.next();
+  }
+
+  if ((!refreshToken && !accessToken) || (!refreshToken && accessToken)) {
     return NextResponse.redirect(
       new URL('/login?alert=로그인 후 이용 가능한 서비스입니다.', request.url),
     );
