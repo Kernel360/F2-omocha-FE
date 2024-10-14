45a5d98581e95f4d117748e041f5ec1b253260c4
Daeeui Kim <kimeodml@gmail.com>
2024-10-14 17:43:04 +0900

    feat: 거래 내역 페이지 추가

---
+++ src/app/mypage/record/components/BasicSold.css.ts
@@ -0,0 +1,66 @@
+import { style, globalStyle, styleVariants } from '@vanilla-extract/css';
+
+export const basicSold = style({
+  display: 'flex',
+  flexDirection: 'column',
+  listStyle: 'none',
+  maxHeight: '400px',
+  overflow: 'scroll',
+});
+
+export const list = style({
+  display: 'flex',
+  gap: '15px',
+  marginBottom: '15px',
+  cursor: 'pointer',
+});
+
+globalStyle(`${list}:nth-last-child(1)`, {
+  marginBottom: '0px',
+});
+
+export const listRight = style({
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-between',
+  listStyle: 'none',
+});
+
+export const listName = style({
+  display: 'inline-block',
+  fontSize: '14px',
+  width: '40px',
+  textAlign: 'end',
+  marginRight: '10px',
+});
+
+export const listValue = style({
+  fontWeight: '500',
+});
+
+export const soldStatus = styleVariants({
+  default: [
+    listValue,
+    {
+      color: 'black',
+    },
+  ],
+  bidding: [
+    listValue,
+    {
+      color: 'red',
+    },
+  ],
+  concluded: [
+    listValue,
+    {
+      color: 'rgb(22, 234, 94)',
+    },
+  ],
+  defeat: [
+    listValue,
+    {
+      color: 'rgb(95, 99, 104)',
+    },
+  ],
+});
--- src/app/mypage/record/page.tsx
+++ src/app/mypage/record/page.tsx
@@ -1,7 +1,34 @@
+import BasicBid from '@/app/mypage/record/components/basicbid';
+import BasicSold from '@/app/mypage/record/components/basicsold';
+import TabsLayout from '@/components/TabsLayout';
+
+import * as S from './Record.css';
+
+const TABS = [
+  {
+    title: '입찰 내역',
+    value: 'basicBid',
+  },
+  {
+    title: '판매 내역',
+    value: 'basicSold',
+  },
+];
+
+const TABS_CONTENT = [<BasicBid key="basicBid" />, <BasicSold key="basicSold" />];
+
 function Home() {
   return (
-    <div>
-      <span>거래 내역</span>
+    <div className={S.record}>
+      <h2>거래 내역</h2>
+      <section className={S.section}>
+        <h3>일반 경매</h3>
+        <TabsLayout
+          defaultTriggerValue={TABS[0].value}
+          triggerTitleList={TABS}
+          childrenList={TABS_CONTENT}
+        />
+      </section>
     </div>
   );
 }
