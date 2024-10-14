14f598add12ef7837d0738755c483c6f0735c7cf
Daeeui Kim <kimeodml@gmail.com>
2024-10-14 15:40:27 +0900

    feat: 키워드 페이지 추가

---
+++ src/app/mypage/keyword/Keyword.css.ts
@@ -0,0 +1,65 @@
+import { style, globalStyle } from '@vanilla-extract/css';
+
+export const keyword = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '30px',
+});
+
+export const section = style({
+  display: 'flex',
+});
+
+export const form = style({
+  display: 'flex',
+  gap: '10px',
+});
+
+export const input = style({
+  flex: '1',
+  borderRadius: '4px',
+  padding: '12px',
+  border: '1.5px solid black',
+});
+
+export const formButton = style({
+  width: '80px',
+  backgroundColor: 'black',
+  color: 'white',
+  borderRadius: '4px',
+});
+
+export const keywordContainer = style({
+  flex: '1',
+  flexDirection: 'column',
+  listStyle: 'none',
+  border: '1.5px solid black',
+  borderRadius: '4px',
+  padding: '15px',
+  maxHeight: '200px',
+  height: '100%',
+  overflow: 'scroll',
+});
+
+export const keywordList = style({
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  border: '1px solid gray',
+  padding: '10px',
+  marginBottom: '15px',
+  cursor: 'pointer',
+
+  ':hover': {
+    border: '1px solid black',
+    boxShadow: '0 0 8px rgba(0, 0, 0, 0.1)',
+  },
+});
+
+globalStyle(`${keywordList}:nth-last-child(1)`, {
+  marginBottom: '0px',
+});
+
+export const deleteButton = style({
+  cursor: 'pointer',
+});
--- src/app/mypage/keyword/page.tsx
+++ src/app/mypage/keyword/page.tsx
@@ -1,7 +1,80 @@
+'use client';
+
+import { useForm } from 'react-hook-form';
+import DeleteIcon from '@/assets/svg/delete.svg';
+
+import * as S from './Keyword.css';
+
+const KEYWORDS = [
+  {
+    id: 1,
+    text: '피규어',
+  },
+  {
+    id: 2,
+    text: '인형',
+  },
+  {
+    id: 3,
+    text: '해파리',
+  },
+  {
+    id: 4,
+    text: '솜사탕',
+  },
+  {
+    id: 5,
+    text: '솜사탕',
+  },
+  {
+    id: 6,
+    text: '솜사탕',
+  },
+];
+
+type Input = {
+  keywordRequired: string;
+};
+
 function Home() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Input>();
+
   return (
-    <div>
-      <span>키워드</span>
+    <div className={S.keyword}>
+      <h2>키워드</h2>
+      <section>
+        <form className={S.form}>
+          <input
+            className={S.input}
+            id="currentPw"
+            type="password"
+            placeholder="알림 받을 키워드를 입력해 주세요."
+            {...register('keywordRequired', {
+              required: '키워드를 입력해 주세요.',
+            })}
+          />
+          <button className={S.formButton} type="submit">
+            등록
+          </button>
+        </form>
+      </section>
+      <section>
+        <ul className={S.keywordContainer}>
+          {KEYWORDS.map(keyword => (
+            <li className={S.keywordList} key={keyword.id}>
+              {keyword.text}
+              <button className={S.deleteButton} type="button">
+                <DeleteIcon />
+              </button>
+            </li>
+          ))}
+        </ul>
+      </section>
     </div>
   );
 }
