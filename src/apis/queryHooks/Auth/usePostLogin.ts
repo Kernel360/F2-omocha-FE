a5752b1856ec2f7023cc5abb8c9ffbe472c5a37c
Daeeui Kim <kimeodml@gmail.com>
2024-10-14 10:45:14 +0900

    feat: 로그인 실패 시 clearToken() 호출

--- src/apis/queryHooks/Auth/usePostLogin.ts
+++ src/apis/queryHooks/Auth/usePostLogin.ts
@@ -9,7 +9,7 @@ import { useTokenStore } from '@/store/token';
 
 function usePostLogin() {
   const router = useRouter();
-  const { setToken } = useTokenStore();
+  const { setToken, clearToken } = useTokenStore();
 
   const { mutate, error } = useMutation({
     mutationFn: (param: LoginParams) => postLogin(param),
@@ -26,8 +26,7 @@ function usePostLogin() {
         console.log('알 수 없는 오류 발생', e.message);
         alert('알 수 없는 오류가 발생했습니다.');
       }
-
-      // 토큰 제거
+      clearToken();
     },
   });
 
