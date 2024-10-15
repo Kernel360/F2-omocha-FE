cc615d711e420a07c5fc821c69b1ea747d20a8f3
dbshaejin <dbshaejin@gmail.com>
2024-10-15 13:01:22 +0900

    design: 디자인 수정

--- src/components/AuctionImageInfo/AuctionImageInfo.css.ts
+++ src/components/AuctionImageInfo/AuctionImageInfo.css.ts
@@ -21,6 +21,7 @@ export const subImageWrapperButton = styleVariants({
     alignItems: 'center',
     border: '1px solid black',
     cursor: 'pointer',
+    borderSizing: 'border-box',
   },
   nonActive: {
     display: 'flex',
@@ -28,6 +29,7 @@ export const subImageWrapperButton = styleVariants({
     alignItems: 'center',
     border: 'none',
     cursor: 'pointer',
+    borderSizing: 'border-box',
   },
 });
 
