f46654a8cf425e0628deb8b18f993369032e231c
Daeeui Kim <kimeodml@gmail.com>
2024-10-15 15:16:15 +0900

    [FEAT] 마이 페이지 레이아웃 구현 (#60)
    * feat: 유저 이름, 좋아요 갯수 표시
    
    * feat: 회원 정보 수정 페이지 추가
    
    * feat: 찜 페이지 추가
    
    * feat: 키워드 페이지 추가
    
    * feat: 거래 내역 페이지 추가
    
    * style: minHeight 수정, borderRight 제거
    
    * fix: lint error 수정
    
    * fix: lint error 수정
    
    * design: 스타일 수정
    
    * fix: AuctionStatusType 제거

---
+++ src/app/mypage/record/Record.css.ts
@@ -0,0 +1,13 @@
+import { style } from '@vanilla-extract/css';
+
+export const record = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '30px',
+});
+
+export const section = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '15px',
+});
---
+++ src/app/mypage/record/components/basicsold.tsx
@@ -0,0 +1,109 @@
+import Image from 'next/image';
+
+import * as S from './BasicSold.css';
+
+const LISTS = [
+  {
+    id: 1,
+    title: '팔아요',
+    image: 'https://via.placeholder.com/280x200',
+    soldStatus: '판매 완료',
+    startPice: 10000,
+    currentPrice: 20000,
+    endDate: '2024-10-14 11:43:34',
+  },
+  {
+    id: 2,
+    title: '팔아요',
+    image: 'https://via.placeholder.com/280x200',
+    soldStatus: '유찰',
+    startPice: 10000,
+    currentPrice: 0,
+    endDate: '2024-10-14 11:43:34',
+  },
+  {
+    id: 3,
+    title: '팔아요',
+    image: 'https://via.placeholder.com/280x200',
+    soldStatus: '판매중',
+    startPice: 10000,
+    currentPrice: 30000,
+    endDate: '2024-10-14 11:43:34',
+  },
+  {
+    id: 5,
+    title: '팔아요',
+    image: 'https://via.placeholder.com/280x200',
+    soldStatus: '판매중',
+    startPice: 10000,
+    currentPrice: 20000,
+    endDate: '2024-10-14 11:43:34',
+  },
+];
+
+export default function BasicSold() {
+  const getStatusStyle = (soldStatus: string) => {
+    if (soldStatus === '판매 완료') {
+      return S.soldStatus.concluded;
+    }
+
+    if (soldStatus === '판매중') {
+      return S.soldStatus.bidding;
+    }
+
+    if (soldStatus === '유찰') {
+      return S.soldStatus.defeat;
+    }
+
+    return S.soldStatus.default;
+  };
+
+  return (
+    <ul className={S.basicSold}>
+      {LISTS.map(list => {
+        const priceDifference = list.currentPrice - list.startPice;
+        const newPriceDifference = priceDifference > 0 ? priceDifference : 0;
+
+        return (
+          <li className={S.list} key={list.id}>
+            <Image
+              className={S.image}
+              src={list.image}
+              width={0}
+              height={0}
+              sizes="100vw"
+              alt="경매 사진"
+            />
+            <ul className={S.listRight}>
+              <li>
+                <span className={S.listName}>상품명</span>
+                <span className={S.listValue}>{list.title}</span>
+              </li>
+              <li>
+                <span className={S.listName}>상태</span>
+                <span className={`${S.listValue} ${getStatusStyle(list.soldStatus)}`}>
+                  {list.soldStatus}
+                </span>
+              </li>
+              <li>
+                <span className={S.listName}>시작가</span>
+                <span className={S.listValue}>{`${list.startPice} 원`}</span>
+              </li>
+              <li>
+                <span className={S.listName}>현재가</span>
+                <span className={S.listValue}>{`${list.currentPrice} 원 `}</span>
+                <span
+                  className={`${S.listValue} ${getStatusStyle(list.soldStatus)}`}
+                >{`(+${newPriceDifference}원)`}</span>
+              </li>
+              <li>
+                <span className={S.listName}>종료</span>
+                <span className={S.listValue}>{list.endDate}</span>
+              </li>
+            </ul>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
