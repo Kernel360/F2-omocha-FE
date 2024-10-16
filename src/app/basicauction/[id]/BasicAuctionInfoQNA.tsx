c182d1d42b934c6e7f6d25776ef00de3fc5df1cb
dbshaejin <dbshaejin@gmail.com>
2024-10-16 17:38:18 +0900

    feat: 아코디언 컴포넌트 더미 구현

--- src/app/basicauction/[id]/BasicAuctionInfoQNA.tsx
+++ src/app/basicauction/[id]/BasicAuctionInfoQNA.tsx
@@ -1,4 +1,5 @@
 import useGetAuctionQNAList from '@/apis/queryHooks/basicAuction/useGetBasicAutionQnAList';
+import AccordionList from '@/components/Accordion';
 
 interface BasicAuctionInfoQNAProps {
   id: number;
@@ -8,10 +9,12 @@ function BasicAuctionInfoQNA({ id }: BasicAuctionInfoQNAProps) {
   console.log(id); // 임시 뺄수도
   const { data } = useGetAuctionQNAList(id);
   console.log('data QnA 내용임.', data);
+
   return (
     <>
       <div>BasicAuctionInfoQNA</div>
       <div>Q</div>
+      <AccordionList />
     </>
   );
 }
