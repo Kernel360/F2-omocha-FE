42b82081eaaa67d2caf18ff53cbc2d8a8fd48a7a
Daeeui Kim <kimeodml@gmail.com>
2024-10-17 14:21:10 +0900

    fix: isLoggedIn으로 변경 및 불필요한 파일 제거

--- src/components/AuctionInfo/AuctionBidListModal.tsx
+++ src/components/AuctionInfo/AuctionBidListModal.tsx
@@ -1,7 +1,7 @@
 import { useRouter } from 'next/navigation';
 
 import useGetBasicAuctionBidList from '@/apis/queryHooks/basicAuction/useGetBasicAuctionBidList';
-import { useTokenStore } from '@/store/token';
+import { useAuth } from '@/provider/authProvider';
 
 import * as S from './AuctionBidListModal.css';
 
@@ -10,7 +10,7 @@ interface AuctionBidListModalProps {
 }
 
 function AuctionBidListModal({ id }: AuctionBidListModalProps) {
-  const { refresh } = useTokenStore();
+  const { isLoggedIn } = useAuth();
   const router = useRouter();
 
   const { data } = useGetBasicAuctionBidList(id);
@@ -19,7 +19,7 @@ function AuctionBidListModal({ id }: AuctionBidListModalProps) {
     router.push('/login');
   };
 
-  if (!refresh) {
+  if (!isLoggedIn) {
     return (
       <div className={S.needLoginSection}>
         <div className={S.noUserMessage}>로그인이 필요한 서비스 입니다.</div>
--- src/components/AuctionInfo/hooks/usePermissionBidPrice.ts
+++ src/components/AuctionInfo/hooks/usePermissionBidPrice.ts
@@ -1,17 +1,18 @@
 import { useState } from 'react';
 
-import useUserStore from '@/store/useUserStore';
+import useGetUser from '@/apis/queryHooks/User/useGetUser';
+import { useAuth } from '@/provider/authProvider';
 
 export function usePermissionBidPrice(sellerId: number) {
-  const user = useUserStore(state => state.user);
-
+  const { isLoggedIn } = useAuth();
+  const { data: user } = useGetUser();
   const [expired, setExpired] = useState(false);
 
   const canNotBid = () => {
     if (expired) {
       return '경매 진행 기간이 아닙니다.';
     }
-    if (!user) {
+    if (!isLoggedIn) {
       return '로그인 후 사용 가능한 서비스입니다.';
     }
     return '';
