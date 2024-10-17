f52ed1c0408c2c4c9fe53b00c77e6dae47ecd0e7
dbshaejin <dbshaejin@gmail.com>
2024-10-17 11:54:39 +0900

    feat: qna 쓰기 ui 구현

--- src/app/basicauction/[id]/BasicAuctionInfoQNA.css.ts
+++ src/app/basicauction/[id]/BasicAuctionInfoQNA.css.ts
@@ -1,4 +1,126 @@
-import { style } from '@vanilla-extract/css';
+import { style, styleVariants } from '@vanilla-extract/css';
+
+export const qnaPostButton = style({
+  display: 'block',
+  padding: '10px',
+  fontSize: '14px',
+  backgroundColor: 'rgb(245, 245, 245)',
+  color: 'black',
+  border: 'none',
+  borderRadius: '5px',
+  cursor: 'pointer',
+  margin: '10px 0',
+  marginLeft: 'auto',
+});
+
+export const accordionPostQnAContent = style({
+  overflow: 'hidden',
+  display: 'flex',
+  flexDirection: 'column',
+  width: '100%',
+  boxSizing: 'border-box',
+  fontSize: '15px',
+  color: 'black',
+  backgroundColor: 'var(--accordion-bg-color)',
+  transition: 'background-color 0.3s',
+
+  selectors: {
+    '&[data-state="open"]': {
+      padding: '10px 20px',
+    },
+  },
+});
+
+export const postQnASection = style({
+  display: 'flex',
+  width: '100%',
+  flexDirection: 'column',
+  gap: '20px',
+});
+
+export const postQnAWrapper = style({
+  display: 'flex',
+  alignItems: 'center',
+  gap: '10px',
+});
+
+export const postQnAWrapperTop = style({
+  display: 'flex',
+
+  gap: '10px',
+});
+
+export const postQnALable = style({
+  fontSize: '14px',
+  width: '80px',
+});
+
+export const postQnAText = style({
+  fontSize: '14px',
+  padding: '5px',
+  backgroundColor: 'rgb(245, 245, 245)',
+});
+
+export const buttonWrapper = style({
+  display: 'flex',
+  gap: '10px',
+  marginLeft: 'auto',
+});
+
+export const postQnAButton = styleVariants({
+  close: [
+    {
+      width: '72px',
+      height: '35px',
+      fontSize: '14px',
+      backgroundColor: 'rgb(245, 245, 245)',
+      borderRadius: '5px',
+      cursor: 'pointer',
+      color: 'rgb(0, 0, 0)',
+      background: 'rgb(255, 255, 255)',
+      border: '1px solid rgb(212, 212, 212)',
+    },
+  ],
+  submit: [
+    {
+      width: '72px',
+      height: '35px',
+      fontSize: '14px',
+      backgroundColor: 'rgb(48, 48, 51)',
+      color: 'rgb(255, 255, 255)',
+      border: '1px solid rgb(48, 48, 51)',
+      borderRadius: '5px',
+      cursor: 'pointer',
+    },
+  ],
+});
+
+export const postQnATextAreaContent = style({
+  padding: '5px',
+  width: '90%',
+  height: '100px',
+  borderRadius: '5px',
+  fontSize: '14px',
+});
+
+export const postQnANoticeSection = style({
+  paddingLeft: '90px',
+  marginBottom: '20px',
+  color: 'rgb(160, 160, 160)',
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '10px',
+});
+
+export const postQnANoticeTitle = style({
+  fontSize: '14px',
+  fontWeight: 'bold',
+});
+
+export const postQnANoticeUnit = style({
+  fontSize: '14px',
+  listStylePosition: 'inside',
+});
 
 export const accordionContainer = style({
   width: '100%',
@@ -11,6 +133,26 @@ export const accordionItem = style({
   ':hover': {
     backgroundColor: 'rgb(245, 245, 245)',
   },
+  selectors: {
+    '&[data-state="open"]': {
+      backgroundColor: 'rgb(245, 245, 245)',
+
+      vars: {
+        '--accordion-bg-color': 'rgb(245, 245, 245)',
+      },
+    },
+    '&[data-state="closed"]': {
+      vars: {
+        '--accordion-bg-color': 'transparent',
+      },
+    },
+  },
+});
+
+export const accordionItemButton = style({
+  width: '100%',
+  borderBottom: '1px solid #e0e0e0',
+
   selectors: {
     '&[data-state="open"]': {
       backgroundColor: 'rgb(245, 245, 245)',
@@ -48,17 +190,63 @@ export const accordionTrigger = style({
   transition: 'background-color 0.3s',
 });
 
+export const userEmail = style({
+  fontSize: '12px',
+  color: 'gray',
+});
+
+export const createAt = style({
+  fontSize: '12px',
+  color: 'gray',
+});
+
+export const haveAnswerBase = style({
+  fontSize: '12px',
+  padding: '5px',
+  color: 'white',
+});
+
+export const haveAnswerVariants = styleVariants({
+  yes: [
+    haveAnswerBase,
+    {
+      backgroundColor: 'rgb(55, 95, 255)',
+    },
+  ],
+  no: [
+    haveAnswerBase,
+    {
+      backgroundColor: 'RGB(255, 95, 55)',
+    },
+  ],
+});
+
 export const accordionContent = style({
   overflow: 'hidden',
   display: 'flex',
+  flexDirection: 'column',
   width: 'fit-content',
   fontSize: '15px',
   color: 'black',
   backgroundColor: 'var(--accordion-bg-color)',
   transition: 'background-color 0.3s',
+
+  selectors: {
+    '&[data-state="open"]': {
+      padding: '10px 20px',
+    },
+  },
 });
 
 export const accordionAuthorContent = style({
   display: 'flex',
+  alignItems: 'center',
   gap: '15px',
+});
+
+export const answerTitle = style({
+  fontSize: '15px',
+  fontWeight: 'bold',
+  color: 'rgb(55, 95, 255)',
+  margin: '10px 0',
 });
--- src/app/basicauction/[id]/BasicAuctionInfoQNA.tsx
+++ src/app/basicauction/[id]/BasicAuctionInfoQNA.tsx
@@ -12,35 +12,84 @@ interface BasicAuctionInfoQNAProps {
 }
 
 function BasicAuctionInfoQNA({ id, isSeller }: BasicAuctionInfoQNAProps) {
-  console.log('isSeller', isSeller); // 임시 뺄수도
-
+  const sampleUserId = 'dbshaejin@gmail.com';
   const { data } = useGetAuctionQNAList(id);
+
   if (!data) return null;
 
-  console.log('data QnA 내용임.', data.result_data.content);
-
   return (
-    <Accordion.Root className={S.accordionContainer} type="single" collapsible>
-      {data.result_data.content.map((item: AuctionQNAData) => (
-        <Accordion.Item
-          key={item.question_response.question_id}
-          value={String(item.question_response.question_id)}
-          className={S.accordionItem}
-        >
-          <AccordionTrigger className={S.accordionTrigger}>
-            <div>{item.question_response.title}</div>
-            <div className={S.accordionAuthorContent}>
-              <div>{item.question_response.email}</div>
-              <div>{item.question_response.created_at}</div>
-              <div>{item.answer_response ? 'yes' : 'no'}</div>
-            </div>
-          </AccordionTrigger>
-          <AccordionContent className={S.accordionContent}>
-            {item.question_response.content}
-          </AccordionContent>
-        </Accordion.Item>
-      ))}
-    </Accordion.Root>
+    <div className={S.accordionContainer}>
+      {isSeller && (
+        <Accordion.Root type="single" collapsible>
+          <Accordion.Item value={'postQNATrigger'} className={S.accordionItemButton}>
+            <AccordionTrigger className={S.qnaPostButton}>QnA 쓰기</AccordionTrigger>
+            <AccordionContent className={S.accordionPostQnAContent}>
+              <>
+                <form className={S.postQnASection}>
+                  <div className={S.postQnAWrapper}>
+                    <label htmlFor="userId" className={S.postQnALable}>
+                      이메일
+                    </label>
+                    <span className={S.postQnAText}>{sampleUserId}</span>
+                  </div>
+                  <div className={S.postQnAWrapperTop}>
+                    <label htmlFor="content" className={S.postQnALable}>
+                      문의내용
+                    </label>
+                    <textarea id="content" className={S.postQnATextAreaContent} />
+                  </div>
+                  <div className={S.buttonWrapper}>
+                    <AccordionTrigger>
+                      <button type="button" className={S.postQnAButton.close}>
+                        닫기
+                      </button>
+                    </AccordionTrigger>
+                    <button type="submit" className={S.postQnAButton.submit}>
+                      등록
+                    </button>
+                  </div>
+                </form>
+                <ul className={S.postQnANoticeSection}>
+                  <span className={S.postQnANoticeTitle}>QnA 작성시 유의사항</span>
+                  <li className={S.postQnANoticeUnit}>
+                    현재 비밀글 서비스가 제공되지 않습니다. QnA작성 시 이메일 전화번호 등 개인
+                    정보를 주의해주세요.
+                  </li>
+                </ul>
+              </>
+            </AccordionContent>
+          </Accordion.Item>
+        </Accordion.Root>
+      )}
+      <Accordion.Root type="single" collapsible>
+        {data.result_data.content.map((item: AuctionQNAData) => (
+          <Accordion.Item
+            key={item.question_response.question_id}
+            value={String(item.question_response.question_id)}
+            className={S.accordionItem}
+          >
+            <AccordionTrigger className={S.accordionTrigger}>
+              <div>{item.question_response.title}</div>
+              <div className={S.accordionAuthorContent}>
+                <div className={S.userEmail}>{item.question_response.email}</div>
+                <div className={S.createAt}>{item.question_response.created_at}</div>
+                <div
+                  className={
+                    item.answer_response ? S.haveAnswerVariants.yes : S.haveAnswerVariants.yes
+                  }
+                >
+                  {item.answer_response ? 'yes' : 'no'}
+                </div>
+              </div>
+            </AccordionTrigger>
+            <AccordionContent className={S.accordionContent}>
+              <span className={S.answerTitle}>답변.</span>
+              {item.question_response.content}
+            </AccordionContent>
+          </Accordion.Item>
+        ))}
+      </Accordion.Root>
+    </div>
   );
 }
 
