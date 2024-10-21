401ca141f45a17915d5bb5630a0aaa9cc91f8bc6
Daeeui Kim <kimeodml@gmail.com>
2024-10-21 16:35:30 +0900

    fix: 함수로 변경

--- src/hooks/useScrollToTop.ts
+++ src/hooks/useScrollToTop.ts
@@ -1,15 +1,13 @@
-'use client';
-
-import { useEffect } from 'react';
-
 function useScrollToTop() {
-  useEffect(() => {
+  const scrollToTop = () => {
     document.documentElement.scrollTo({
       top: 0,
       left: 0,
       behavior: 'smooth',
     });
-  }, []);
+  };
+
+  return { scrollToTop };
 }
 
 export default useScrollToTop;
