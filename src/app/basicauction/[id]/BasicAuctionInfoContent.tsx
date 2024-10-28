1d633a8a218dcf449b8049c88c55e51a4df8ecdf
Daeeui Kim <kimeodml@gmail.com>
2024-10-28 20:05:21 +0900

    feat: slate의 구조를 html로 변경

--- src/app/basicauction/[id]/BasicAuctionInfoContent.tsx
+++ src/app/basicauction/[id]/BasicAuctionInfoContent.tsx
@@ -1,9 +1,15 @@
+import { convertSlateToHTML } from '@/utils/convertSlateToHTML';
+
 interface BasicAuctionInfoContentProps {
   content: string;
 }
 
 function BasicAuctionInfoContent({ content }: BasicAuctionInfoContentProps) {
-  return <div>{content}</div>;
+  const contentArray = JSON.parse(content);
+
+  const htmlContent = convertSlateToHTML(contentArray);
+
+  return <div dangerouslySetInnerHTML={{ __html: htmlContent }} />;
 }
 
 export default BasicAuctionInfoContent;
