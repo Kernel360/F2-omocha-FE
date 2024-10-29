74255687e87a46b1d5c0762cefab7950cb6168cb
dbshaejin <dbshaejin@gmail.com>
2024-10-29 11:04:23 +0900

    feat: 디자인 관련 요소 파일 추가

---
+++ src/styles/color.ts
@@ -0,0 +1,67 @@
+const primary = {
+  primary1: '#fcfdfa',
+  primary2: '#f7faf3',
+  primary3: '#eaf8d7',
+  primary4: '#ddf2be',
+  primary5: '#cfe9a6',
+  primary6: '#bedb8f',
+  primary7: '#aaca73',
+  primary8: '#90b545',
+  primary9: '#a0d30f',
+  primary10: '#96c800',
+  primary11: '#5e7b1d',
+  primary12: '#33401a',
+};
+
+const secondary = {
+  secondary1: '#fcfdff',
+  secondary2: '#f7f8ff',
+  secondary3: '#eff1ff',
+  secondary4: '#e5e8ff',
+  secondary5: '#d8ddff',
+  secondary6: '#c9ceff',
+  secondary7: '#b5bbf8',
+  secondary8: '#9aa0f1',
+  secondary9: '#7b7bff',
+  secondary10: '#706fee',
+  secondary11: '#5e5ad6',
+  secondary12: '#2b2a64',
+};
+
+const gray = {
+  gray1: '#fcfcfd',
+  gray2: '#f9f9fb',
+  gray3: '#eff0f3',
+  gray4: '#e7e8ec',
+  gray5: '#e0e1e6',
+  gray6: '#d8d9e0',
+  gray7: '#cdced7',
+  gray8: '#b9bbc6',
+  gray9: '#8b8d98',
+  gray10: '#80828d',
+  gray11: '#62636c',
+  gray12: '#1e1f24',
+};
+
+const black = {
+  black1: '#000000',
+};
+
+const white = {
+  white: '#ffffff',
+};
+
+const red = {
+  red: '#FF0000',
+};
+
+const colors = {
+  ...primary,
+  ...secondary,
+  ...gray,
+  ...black,
+  ...white,
+  ...red,
+};
+
+export default colors;
---
+++ src/styles/typo.ts
@@ -0,0 +1,68 @@
+const fontSize = {
+  h1: '32px',
+  h2: '28px',
+  h3: '24px',
+  h4: '20px',
+  bodyLarge: '16px',
+  body: '14px',
+  caption: '12px',
+};
+
+// Font Weight
+const fontWeight = {
+  regular: 400,
+  medium: 500,
+  bold: 700,
+};
+
+// Line Height
+const lineHeight = {
+  h1: '44px',
+  h2: '38px',
+  h3: '32px',
+  h4: '28px',
+  bodyLarge: '24px',
+  body: '20px',
+  caption: '16px',
+};
+
+// Typography
+const typography = {
+  h1: {
+    fontSize: fontSize.h1,
+    fontWeight: fontWeight.bold,
+    lineHeight: lineHeight.h1,
+  },
+  h2: {
+    fontSize: fontSize.h2,
+    fontWeight: fontWeight.bold,
+    lineHeight: lineHeight.h2,
+  },
+  h3: {
+    fontSize: fontSize.h3,
+    fontWeight: fontWeight.medium,
+    lineHeight: lineHeight.h3,
+  },
+  h4: {
+    fontSize: fontSize.h4,
+    fontWeight: fontWeight.medium,
+    lineHeight: lineHeight.h4,
+  },
+  bodyLarge: {
+    fontSize: fontSize.bodyLarge,
+    fontWeight: fontWeight.regular,
+    lineHeight: lineHeight.bodyLarge,
+  },
+  body: {
+    fontSize: fontSize.body,
+    fontWeight: fontWeight.regular,
+    lineHeight: lineHeight.body,
+  },
+  caption: {
+    fontSize: fontSize.caption,
+    fontWeight: fontWeight.regular,
+    lineHeight: lineHeight.caption,
+  },
+};
+
+export default typography;
