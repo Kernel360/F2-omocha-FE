fe74c61bfe0335f04a2f591665b837e9c0546ed8
Daeeui Kim <kimeodml@gmail.com>
2024-11-05 13:49:28 +0900

    feat: AuctionListSkeletonUI 추가

---
+++ src/components/SkeletonUI/AuctionListSkeletonUI.tsx
@@ -0,0 +1,30 @@
+/* eslint-disable react/no-array-index-key */
+import ListLayout from '../ListLayout';
+
+import * as S from './AuctionListSkeletonUI.css';
+import SkeletonCard from './components/SkeletonCard';
+import SkeletonText from './components/SkeletonText';
+
+interface AuctionListSkeletonUIProps {
+  number?: number;
+}
+
+function AuctionListSkeletonUI({ number }: AuctionListSkeletonUIProps) {
+  return (
+    <div className={S.container}>
+      <SkeletonCard width={82} height={30} />
+      <ListLayout>
+        {Array.from({ length: number || 3 }).map((_, index) => (
+          <div key={index} className={S.cardContainer}>
+            <SkeletonCard width={186} height={196} />
+            <div className={S.textContainer}>
+              <SkeletonText noOfLines={2} gap={8} />
+            </div>
+          </div>
+        ))}
+      </ListLayout>
+    </div>
+  );
+}
+
+export default AuctionListSkeletonUI;
