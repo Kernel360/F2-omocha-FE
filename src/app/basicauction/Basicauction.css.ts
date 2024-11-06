f099b8e3e262f5cc1df4cc96dc5e2851502739e9
dbshaejin <dbshaejin@gmail.com>
2024-11-06 12:35:29 +0900

    chore: 충돌 해결

--- src/app/basicauction/Basicauction.css.ts
+++ src/app/basicauction/Basicauction.css.ts
@@ -3,35 +3,7 @@ import { style } from '@vanilla-extract/css';
 export const basicAuctionContainer = style({
   display: 'flex',
   gap: '20px',
-<<<<<<< HEAD
   padding: '60px 0',
-});
-
-export const leftSection = style({
-  display: 'flex',
-  flexDirection: 'column',
-  gap: '30px',
-});
-
-export const count = style({
-  display: 'flex',
-  alignItems: 'center',
-  gap: '5px',
-});
-
-globalStyle(`${count} span:nth-child(1)`, typography.bodyLarge);
-
-globalStyle(`${count} span:nth-child(2)`, {
-  ...typography.body,
-  display: 'flex',
-  justifyContent: 'center',
-  color: colors.primary11,
-  backgroundColor: colors.gray3,
-  minWidth: '30px',
-  padding: '5px 10px',
-  borderRadius: '20px',
-=======
->>>>>>> develop
 });
 
 export const rightSection = style({
