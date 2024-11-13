fb7bc2f640d259120eb84db3af84714f1861f9bb
Daeeui Kim <kimeodml@gmail.com>
2024-11-13 15:36:43 +0900

    fix: 컴포넌트 구조 변경

--- src/components/MypageProfileClientPage/components/ImageUploadModal.css.ts
+++ src/components/MypageProfileClientPage/components/ImageUploadModal.css.ts
@@ -2,24 +2,24 @@ import { style } from '@vanilla-extract/css';
 
 import colors from '@/styles/color';
 
-export const imageWrapper = style({
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  borderRadius: '50%',
-  color: colors.gray5,
-  border: `3px solid ${colors.gray5}`,
-});
-
 export const imageProfile = style({
   borderRadius: '50%',
 });
 
 export const image = style({
   display: 'flex',
-  position: 'relative',
   justifyContent: 'center',
   marginBottom: '15px',
+});
+
+export const imageWrapper = style({
+  position: 'relative',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  borderRadius: '50%',
+  color: colors.gray5,
+  border: `3px solid ${colors.gray5}`,
 });
 
 export const imageUpload = style({
@@ -35,15 +35,4 @@ export const plusIcon = style({
   ':hover': {
     filter: 'brightness(0.8)',
   },
-});
-
-export const error = style({
-  display: 'flex',
-  justifyContent: 'center',
-  gap: '4px',
-  alignItems: 'center',
-  color: colors.primary10,
-  fontSize: '14px',
-  fontWeight: 'normal',
-  marginBottom: '15px',
 });
--- src/components/MypageProfileClientPage/components/ImageUploadModal.tsx
+++ src/components/MypageProfileClientPage/components/ImageUploadModal.tsx
@@ -1,7 +1,7 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 
-import { UserIcon, CirclePlusIcon, TriangleAlertIcon } from 'lucide-react';
+import { UserIcon, CirclePlusIcon } from 'lucide-react';
 import Image from 'next/image';
 
 import usePatchProfileImage from '@/apis/queryHooks/User/usePatchProfileImage';
@@ -21,27 +21,30 @@ interface ImageUploadModalProps {
 }
 
 function ImageUploadModal({ onClose, defaultImage }: ImageUploadModalProps) {
-  const {
-    handleSubmit,
-    formState: { errors },
-    setValue,
-  } = useForm<Input>({ mode: 'onChange' });
+  const { register, handleSubmit, setValue } = useForm<Input>({ mode: 'onChange' });
 
   const [previewImage, setPreviewImage] = useState<File | null>(null);
   const { mutate: patchProfileImage } = usePatchProfileImage();
 
   const onSubmit: SubmitHandler<Input> = useDebounce(data => {
-    const formData = new FormData();
-    if (data.imageRequired) {
-      formData.append('profileImage', data.imageRequired);
-
-      patchProfileImage(formData);
+    if (
+      !data.imageRequired ||
+      (data.imageRequired instanceof FileList && data.imageRequired.length === 0)
+    ) {
+      return;
     }
+
+    const formData = new FormData();
+
+    formData.append('profileImage', data.imageRequired);
+    patchProfileImage(formData);
+
     onClose();
   }, 500);
 
   const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
+
     if (file) {
       setPreviewImage(file);
       setValue('imageRequired', file);
@@ -51,60 +54,49 @@ function ImageUploadModal({ onClose, defaultImage }: ImageUploadModalProps) {
   const imageRender = () => {
     if (previewImage) {
       return (
-        <div className={S.imageWrapper}>
-          <Image
-            className={S.imageProfile}
-            src={URL.createObjectURL(previewImage)}
-            width={100}
-            height={100}
-            alt="이미지 프로필"
-          />
-        </div>
+        <Image
+          className={S.imageProfile}
+          src={URL.createObjectURL(previewImage)}
+          width={100}
+          height={100}
+          alt="이미지 프로필"
+        />
       );
     }
 
     if (defaultImage) {
       return (
-        <div className={S.imageWrapper}>
-          <Image
-            className={S.imageProfile}
-            src={`https://s3.ap-northeast-2.amazonaws.com/omocha.storages/${defaultImage}`}
-            width={100}
-            height={100}
-            alt="프로필 이미지"
-          />
-        </div>
+        <Image
+          className={S.imageProfile}
+          src={`https://s3.ap-northeast-2.amazonaws.com/omocha.storages/${defaultImage}`}
+          width={100}
+          height={100}
+          alt="프로필 이미지"
+        />
       );
     }
 
-    return (
-      <div className={S.imageWrapper}>
-        <UserIcon size={100} strokeWidth={1} stroke={colors.gray5} />
-      </div>
-    );
+    return <UserIcon size={100} strokeWidth={1} stroke={colors.gray5} />;
   };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <div className={S.image}>
-        {imageRender()}
-        <label htmlFor="이미지" className={S.plusIcon}>
-          <input
-            id="이미지"
-            type="file"
-            accept="image/*"
-            className={S.imageUpload}
-            onChange={handleImageChange}
-          />
-          <CirclePlusIcon size={25} fill={colors.secondary7} stroke={colors.white} />
-        </label>
+        <div className={S.imageWrapper}>
+          {imageRender()}
+          <label htmlFor="이미지" className={S.plusIcon}>
+            <input
+              id="이미지"
+              type="file"
+              accept="image/*"
+              {...register('imageRequired')}
+              className={S.imageUpload}
+              onChange={handleImageChange}
+            />
+            <CirclePlusIcon size={25} fill={colors.secondary7} stroke={colors.white} />
+          </label>
+        </div>
       </div>
-      {errors.imageRequired && (
-        <span className={S.error}>
-          <TriangleAlertIcon size={16} stroke={colors.primary10} />
-          {errors.imageRequired.message}
-        </span>
-      )}
       <CommonButton type="submit" size="md" content="이미지 업로드" />
     </form>
   );
