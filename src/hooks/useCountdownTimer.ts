5f138350e683beded098e5385d2d297324deb0c9
dbshaejin <dbshaejin@gmail.com>
2024-11-21 14:32:07 +0900

    fix: QA 반영 경매 종료 시 타이머를 0

--- src/hooks/useCountdownTimer.ts
+++ src/hooks/useCountdownTimer.ts
@@ -6,13 +6,14 @@ import calcRemainingTime from '@/utils/calcRemainingTime';
 
 interface UseCountdownTimerProps {
   endTime: Date | string;
+  auctionStatus?: string;
 }
 
 const MINUTE_IN_MILLIS = 1000 * 60;
 const HOUR_IN_MILLIS = MINUTE_IN_MILLIS * 60;
 const DAY_IN_MILLIS = HOUR_IN_MILLIS * 24;
 
-function useCountdownTimer({ endTime }: UseCountdownTimerProps) {
+function useCountdownTimer({ endTime, auctionStatus }: UseCountdownTimerProps) {
   const [remainingTime, setRemainingTime] = useState<number>(0);
 
   useEffect(() => {
@@ -39,6 +40,10 @@ function useCountdownTimer({ endTime }: UseCountdownTimerProps) {
   const minute = Math.floor((remainingTime % HOUR_IN_MILLIS) / MINUTE_IN_MILLIS) ?? '-';
   const second = Math.floor((remainingTime % MINUTE_IN_MILLIS) / 1000) ?? '-';
   const isTimeout = remainingTime <= 0;
+
+  if (auctionStatus === 'CONCLUDED' || auctionStatus === 'COMPLETE') {
+    return { isTimeout, remainingTime, day: 0, hour: 0, minute: 0, second: 0 };
+  }
 
   return { isTimeout, remainingTime, day, hour, minute, second };
 }
