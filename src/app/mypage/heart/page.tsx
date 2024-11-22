88f8a0be2ac6a3c3c7209204e928e215e0f3c487
Daeeui Kim <kimeodml@gmail.com>
2024-11-22 10:14:51 +0900

    [REFACTOR] 마이 페이지 수정 (#133)
    * design: 마이페이지 디자인 수정
    
    * design: 모바일 고려하여 수정
    
    * fix: 코드 리뷰 반영
    
    * fix: minHeight 반영

--- src/app/mypage/heart/page.tsx
+++ src/app/mypage/heart/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
+
 import useGetAuctionLikeList from '@/apis/queryHooks/User/useGetAuctionLikeList';
 import useGetUser from '@/apis/queryHooks/User/useGetUser';
 import AuctionCard from '@/components/AuctionCard';
@@ -11,6 +13,7 @@ import * as S from './Heart.css';
 
 function Home() {
   const { data: user } = useGetUser();
+  const router = useRouter();
   const { data, hasNextPage, fetchNextPage, isFetchingNextPage, isLoading } = useGetAuctionLikeList(
     {
       size: 4,
@@ -21,6 +24,8 @@ function Home() {
     hasNextPage,
     fetchNextPage,
   });
+
+  console.log(data);
 
   if (isLoading)
     return (
@@ -33,25 +38,40 @@ function Home() {
   return (
     <div className={S.heart}>
       <h3>찜 목록 ({user?.like_count})</h3>
-      <ListLayout>
-        {data?.pages.map(page =>
-          page.result_data.content.map(item => (
-            <AuctionCard
-              key={item.auction_id}
-              id={item.auction_id}
-              thumbnailImage={item.thumbnail_path}
-              title={item.title}
-              isLike={!!item.liked_date}
-              startPrice={item.start_price}
-              startTime={item.start_date}
-              endTime={item.end_date}
-              nowPrice={item.now_price}
-            />
-          )),
-        )}
-      </ListLayout>
-      {isFetchingNextPage && hasNextPage && <AuctionListSkeletonUI count={4} />}
-      <div ref={endCursorRef} />
+      {data?.pages[0].result_data.total_elements === 0 ? (
+        <div className={S.noListWrapper}>
+          <div className={S.noListTitle}>아직 찜한 물품이 없습니다.</div>
+          <button
+            className={S.noListButton}
+            type="button"
+            onClick={() => router.push('/basicauction', { scroll: false })}
+          >
+            상품 구경하러 가기
+          </button>
+        </div>
+      ) : (
+        <>
+          <ListLayout>
+            {data?.pages.map(page =>
+              page.result_data.content.map(item => (
+                <AuctionCard
+                  key={item.auction_id}
+                  id={item.auction_id}
+                  thumbnailImage={item.thumbnail_path}
+                  title={item.title}
+                  isLike={!!item.liked_date}
+                  startPrice={item.start_price}
+                  startTime={item.start_date}
+                  endTime={item.end_date}
+                  nowPrice={item.now_price}
+                />
+              )),
+            )}
+          </ListLayout>
+          {isFetchingNextPage && hasNextPage && <AuctionListSkeletonUI count={4} />}
+          <div ref={endCursorRef} />
+        </>
+      )}
     </div>
   );
 }
