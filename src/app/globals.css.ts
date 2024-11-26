6da4d425498fa7d49adbd82689943368af2b9528
Daeeui Kim <kimeodml@gmail.com>
2024-11-26 11:25:41 +0900

    design: 경매 내역 minHeight 수정, 주석 추가

--- src/app/globals.css.ts
+++ src/app/globals.css.ts
@@ -13,6 +13,7 @@ globalStyle('html, body', {
   minHeight: '100vh',
 });
 
+// 엣지 블라인드 모드 삭제
 globalStyle('input[type="text"]::-ms-clear, input[type="password"]::-ms-reveal', {
   display: 'none',
 });
