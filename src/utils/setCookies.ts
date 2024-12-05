3de2bf248d6e8124cbd44fedc301a33336692726
dbshaejin <dbshaejin@gmail.com>
2024-12-05 10:44:49 +0900

    fix: 코드리뷰 반영

--- src/utils/setCookies.ts
+++ src/utils/setCookies.ts
@@ -1,7 +1,7 @@
 import { ResponseCookie } from 'next/dist/compiled/@edge-runtime/cookies';
 import { cookies } from 'next/headers';
 
-export const setCookie = (key: string, value: string, options?: Partial<ResponseCookie>) => {
+export const setCookies = (key: string, value: string, options?: Partial<ResponseCookie>) => {
   return cookies().set(key, value, {
     ...options,
     maxAge: 1800, // 30분
