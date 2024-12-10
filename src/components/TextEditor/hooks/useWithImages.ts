e1e042a9a4d478d9c7f5cb6415e9803a299cf67d
dbshaejin <dbshaejin@gmail.com>
2024-12-10 17:20:09 +0900

    feat: api 연동

--- src/components/TextEditor/hooks/useWithImages.ts
+++ src/components/TextEditor/hooks/useWithImages.ts
@@ -1,8 +1,39 @@
 /* eslint-disable no-param-reassign */
+
 import { Editor } from 'slate';
 
+import createFetchApiClient from '@/apis/queryFunctions/createFetchApiClient';
+import { Response } from '@/apis/types/common';
 import { type ImageElement } from '@/components/TextEditor/hooks/useImage';
 import { useToast } from '@/provider/toastProvider';
+
+interface ImageResponseData {
+  image_path: string;
+}
+
+const uploadImage = async (image: File | Blob) => {
+  const imageForm = new FormData();
+
+  imageForm.append('image', image);
+
+  const response = await createFetchApiClient<Response<ImageResponseData>>(`/v2/images`, {
+    method: 'POST',
+    body: imageForm,
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+  });
+
+  // const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_API_URL}/api/v2/images`, {
+  //   method: 'POST',
+  //   headers: {
+  //     'Content-Type': 'multipart/form-data',
+  //   },
+  //   body: imageForm,
+  // }).then(res => res.json());
+
+  return response;
+};
 
 interface WithImagesProps {
   editor: Editor;
@@ -45,6 +76,12 @@ function useWithImages() {
           if (mime === 'image') {
             reader.addEventListener('load', () => {
               const url = reader.result;
+              console.log(files[i]);
+              const imageTest = uploadImage(files[i]);
+              // TODO
+              console.log(imageTest);
+              // 여기서 s3 upload api 호출
+              // return response를 url로
               insertImage(editor, url as string);
             });
 
